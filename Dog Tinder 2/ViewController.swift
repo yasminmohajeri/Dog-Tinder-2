b96be715bc38a607cf218f3d6dc72fd4839da100
Yasmin Mohajeri (s5129439) <s5129439@mac38582.student.bournemouth.ac.uk>
2018-12-04 09:27:17 +0000

    Initial Commit

---
+++ Dog Tinder 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog Tinder 2
+//
+//  Created by Yasmin Mohajeri (s5129439) on 04/12/2018.
+//  Copyright © 2018 Yasmin Mohajeri (s5129439). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
