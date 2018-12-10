4b89054425a4dddcfd21e1af6d74a607ffd996a0
Archie Norton (s5110583) <s5110583@mac42942.student.bournemouth.ac.uk>
2018-12-10 14:04:26 +0000

    Storage Ref Test

--- Dog Tinder 2/ViewController.swift
+++ Dog Tinder 2/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create a root reference
+        let storageRef = Storage.storage().reference()
+        
+        // Create a reference to "mountains.jpg"
+        let testsRef = storageRef.child("test.jpg")
+        
+        // Create a reference to 'images/mountains.jpg'
+        let testImagesRef = storageRef.child("images/tests.jpg")
+        
+        // While the file names are the same, the references point to different files
+        testsRef.name == testImagesRef.name;  // true
+        testsRef.fullPath == testImagesRef.fullPath;  //false
+        
+        let image = UIImage(named: "test")!
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        // Data in memory
+        let imageData = image.jpegData(compressionQuality: 0)
+        
+     
+        testImagesRef.putData(imageData!, metadata: metadata) { metadata, error in
+            guard let metadata = metadata else {
+                print(error?.localizedDescription)
+                return
+            }
+            
+            let size = metadata.size
+            // You can also access to download URL after upload.
+            print(size)
+            
+        }
+        
+
     }
 
 
